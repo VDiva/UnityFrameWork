a7ea7ac3b08aa05b3b8e8fff6154b85faefc86f0
VDiva <45584276+VDiva@users.noreply.github.com>
2024-01-27 19:22:41 +0800

    添加扩展方法

--- Assets/FrameWork/ExtendFunctions/ExtendClass.cs
+++ Assets/FrameWork/ExtendFunctions/ExtendClass.cs
@@ -8,5 +8,23 @@ namespace FrameWork
         {
             mono.gameObject.SetActive(active);
         }
+
+        public static void Rpc(this GameObject gameObject,string methodName,Rpc rpc,object[] parma)
+        {
+            var net = gameObject.GetComponent<NetWorkSystemMono>();
+            if (net!=null)
+            {
+                NetWorkSystem.Rpc(methodName,net,rpc,parma);
+            }
+        }
+        
+        public static void Rpc(this NetWorkSystemMono net,string methodName,Rpc rpc,object[] parma)
+        {
+            //var net = gameObject.GetComponent<NetWorkSystemMono>();
+            if (net!=null)
+            {
+                NetWorkSystem.Rpc(methodName,net,rpc,parma);
+            }
+        }
     }
 }
