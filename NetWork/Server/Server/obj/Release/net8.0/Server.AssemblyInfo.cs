3eafc0e5c53382b46ed352e5ac34f7fcaf5eeb69
VDiva <1806070444@qq.com>
2024-11-24 10:13:05 +0800

    升级编辑器版本

--- NetWork/Server/Server/obj/Release/net8.0/Server.AssemblyInfo.cs
+++ NetWork/Server/Server/obj/Release/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b314bc3781b61564e20523c9f4a8bf86b40f2484")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95cd078b1f696b501c99f35e670608d04f78f1bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
