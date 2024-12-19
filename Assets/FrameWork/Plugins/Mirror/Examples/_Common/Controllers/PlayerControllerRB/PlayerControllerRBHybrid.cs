255397fadef2d7b4871967839f3e59f78e72da5c
VDiva <45584276+VDiva@users.noreply.github.com>
2024-12-19 09:38:14 +0800

    删除插件目录

--- Assets/FrameWork/Plugins/Mirror/Examples/_Common/Controllers/PlayerControllerRB/PlayerControllerRBHybrid.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-namespace Mirror.Examples.Common.Controllers.Player
-{
-    [AddComponentMenu("Network/Player Controller RB (Hybrid)")]
-    [RequireComponent(typeof(NetworkTransformHybrid))]
-    public class PlayerControllerRBHybrid : PlayerControllerRBBase { }
-}
--- Assets/FrameWork/Plugins/Mirror/Transports/Encryption/PubKeyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Mirror.BouncyCastle.Crypto;
-
-public struct PubKeyInfo
-{
-    public string Fingerprint;
-    public ArraySegment<byte> Serialized;
-    public AsymmetricKeyParameter Key;
-}
