18164c99047b1a0fc06b96c491054ad85b5d10e1
VDiva <1806070444@qq.com>
2025-03-15 17:35:46 +0800

    添加一个ui滑动框在数量非常多的情况优化滑动

--- Assets/FrameWork/Tool/UiTool/PassMouseEvent.cs
+++ Assets/FrameWork/Tool/UiTool/PassMouseEvent.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace FrameWork
 {
@@ -11,26 +12,58 @@ namespace FrameWork
         {
             PassEvent(eventData,ExecuteEvents.pointerClickHandler);
         }
-        //
-        // public void OnPointerDown(PointerEventData eventData)
-        // {
-        //     PassEvent(eventData,ExecuteEvents.pointerDownHandler);
-        // }
-        //
-        // public void OnPointerUp(PointerEventData eventData)
-        // {
-        //     PassEvent(eventData,ExecuteEvents.pointerUpHandler);
-        // }
-
+        
+        private static bool hasPassedEvent=false;
         private static void PassEvent<T>(PointerEventData data, ExecuteEvents.EventFunction<T> function) where T: IEventSystemHandler
         {
+            if(hasPassedEvent)
+            {
+                return;
+            }
+            hasPassedEvent = true;
             var result = new List<RaycastResult>();
             EventSystem.current.RaycastAll(data,result);
             var current = data.pointerCurrentRaycast.gameObject;
-            foreach (var t in result.Where(t=>current!=t.gameObject))
+
+            for (int i = 0; i < result.Count; i++)
             {
-                ExecuteEvents.Execute(t.gameObject, data, function);
+                if (result[i].gameObject!=current)
+                {
+                    ExecuteEvents.Execute(result[i].gameObject, data, function);
+                }
             }
+            result.Clear();
+            hasPassedEvent = false;
+        }
+        
+        
+        private static void PassEventOne<T>(PointerEventData data, ExecuteEvents.EventFunction<T> function) where T: IEventSystemHandler
+        {
+            if(hasPassedEvent)
+            {
+                return;
+            }
+            hasPassedEvent = true;
+            var result = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(data,result);
+            var current = data.pointerCurrentRaycast.gameObject;
+            int index = 0;
+            for (int i = 0; i < result.Count; i++)
+            {
+                if (index>0)break;
+                if (result[i].gameObject!=current&& result[i].gameObject.GetComponent<Button>()!=null||result[i].gameObject.GetComponent<Button>()!=null)
+                {
+                    ExecuteEvents.Execute(result[i].gameObject, data, function);
+                }
+                else
+                {
+                    continue;
+                }
+
+                index += 1;
+            }
+            result.Clear();
+            hasPassedEvent = false;
         }
     }
 }
---
+++ Assets/FrameWork/Tool/UiTool/ScrollRectTool.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FrameWork
+{
+    [RequireComponent(typeof(ScrollRect))]
+    public class ScrollRectTool : MonoBehaviour
+    {
+        public GameObject prefab;
+        public int horCount=1;
+        public float space = 5;
+        private RectTransform _prefabRect;
+        private ScrollRect _scrollRect;
+        private RectTransform _scrollRectTransform;
+        private Action<int,GameObject> _scrollItemCallback;
+        private int _count;
+
+        
+        private void Awake()
+        {
+            _scrollRect = GetComponent<ScrollRect>();
+            if (prefab==null&&_scrollRect.content.childCount>0)
+            {
+                prefab=_scrollRect.content.GetChild(0).gameObject;
+            }
+            _prefabRect=prefab.GetComponent<RectTransform>();
+            _scrollRectTransform = _scrollRect.GetComponent<RectTransform>();
+            _scrollRect.horizontal=false;
+            _scrollRect.vertical = true;
+            _scrollRect.content.pivot = new Vector2(0.5f,1);
+            _scrollRect.onValueChanged.AddListener(OnValueChanged);
+        }
+
+        public void Init(int count,Action<int,GameObject> callback=null)
+        {
+            _count = count;
+            _scrollItemCallback = callback;
+            float c = _count;
+            _scrollRect.content.pivot = new Vector2(0.5f,1);
+            var width=(_prefabRect.rect.width+space)*horCount;
+            var height=(_prefabRect.rect.height+space)*(c/horCount);
+            height += _prefabRect.sizeDelta.y;
+            
+            
+            _scrollRect.content.sizeDelta=new Vector2(width,height);
+            OnValueChanged(Vector2.zero);
+            int spawnCount = (int)(_scrollRectTransform.sizeDelta.y / _prefabRect.sizeDelta.y+1)*horCount;
+            for (int i = 0; i < spawnCount; i++)
+            {
+                Transform tran = null;
+                if (i < _scrollRect.content.childCount)
+                    tran = _scrollRect.content.GetChild(i);
+                else
+                    tran = GameObject.Instantiate(prefab, _scrollRect.content).transform;
+                tran.localPosition = GetItemPos(i);
+                _scrollItemCallback?.Invoke(i,tran.gameObject);
+            }
+
+            _fistIndex = 0;
+            _lastIndex=spawnCount-1;
+        }
+        
+        private Vector2 _lastLoc;
+        private int _fistIndex;
+        private int _lastIndex;
+        private void OnValueChanged(Vector2 pos)
+        {
+            bool isUp = _scrollRect.content.localPosition.y > _lastLoc.y;
+            for (int i = 0; i < _scrollRect.content.childCount; i++)
+            {
+                var item = _scrollRect.content.GetChild(i);
+                var loc=_scrollRect.viewport.InverseTransformPoint(item.position);
+                if (i==0)
+                {
+                    Debug.Log(loc+"---"+_scrollRectTransform.sizeDelta+"--"+_fistIndex+"--"+_lastIndex);
+                }
+                if (isUp)
+                {
+                    if (_lastIndex>=_count)return;
+                    if (loc.y> _prefabRect.sizeDelta.y)
+                    {
+                        _fistIndex += 1;
+                        _lastIndex += 1;
+                        item.GetComponent<RectTransform>().anchoredPosition = GetItemPos(_lastIndex);
+                        _scrollItemCallback?.Invoke(_lastIndex,item.gameObject);
+                    }
+                }
+                else
+                {
+                    if (_fistIndex<=0)return;
+                    if (Mathf.Abs(loc.y)>_scrollRectTransform.sizeDelta.y)
+                    {
+                        _fistIndex -= 1;
+                        _lastIndex -= 1;
+                        
+                        item.GetComponent<RectTransform>().anchoredPosition = GetItemPos(_fistIndex);
+                        _scrollItemCallback?.Invoke(_fistIndex,item.gameObject);
+                    }
+                }
+                
+            }
+
+            _lastLoc = _scrollRect.content.localPosition;
+        }
+        
+        
+        public Vector2 GetItemPos(int index)
+        {
+            float c = index;
+            var y = (int)(c / horCount) * (_prefabRect.sizeDelta.y + space);
+            var leftLoc = (horCount-1) / 2f * _prefabRect.sizeDelta.x;
+            var x = -leftLoc + c % horCount * (_prefabRect.sizeDelta.x + space);
+            return new Vector2(x, -y);
+        }
+        
+    }
+}
